a7b8ddfea08b140a30e11046b89de8244bba578e
Gabriel Reis <tharazo101@gmail.com>
2022-02-02 21:58:17 -0300

    Full Template

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal(`.home, .home__img,
+            .sobre, .products,
+            .card, .servicos,
+            .testimonial,
+            .contact__data, .contact__button,
+            .footer__content`, {
+    interval: 200
+})
